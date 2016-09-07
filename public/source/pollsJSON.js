89e293415991096d8ee0eafbad63fb74789f2f2b
Andrew Boyle <ajboyle31@gmail.com>
2016-09-07 19:42:03 +0000

    holds the test poll data

---
+++ public/source/pollsJSON.js
@@ -0,0 +1,61 @@
+var mypolls = [
+  { 
+    "id": 1,
+    "createdBy": "CThePants",
+    "pollname": "Animals",
+    "options": [
+      {
+        "option": "Bird",
+        "votes": 3
+      },
+      {
+        "option": "Dog",
+        "votes": 7
+      },
+      {
+        "option": "Cat",
+        "votes": 10
+      }
+    ]
+  },
+  { 
+    "id": 2,
+    "createdBy": "CThePants",
+    "pollname": "Cars",
+    "options": [
+      {
+        "option": "Truck",
+        "votes": 4
+      },
+      {
+        "option": "Sedan",
+        "votes": 8
+      },
+      {
+        "option": "SUV",
+        "votes": 11
+      }
+    ]
+  },
+  { 
+    "id": 3,
+    "createdBy": "CThePants",
+    "pollname": "Aircraft",
+    "options": [
+      {
+        "option": "Planes",
+        "votes": 2
+      },
+      {
+        "option": "Rocket",
+        "votes": 6
+      },
+      {
+        "option": "Helicopter",
+        "votes": 14
+      }
+    ]
+  }
+];
+
+export default mypolls;
