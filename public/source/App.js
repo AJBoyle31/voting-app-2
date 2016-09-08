61f191edea951c808c4a55c7f6ed40b938522a43
Andrew Boyle <ajboyle31@gmail.com>
2016-09-08 17:49:13 +0000

    removed state and moved to Polls.js

--- public/source/App.js
+++ public/source/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router';
-import mypolls from './pollsJSON.js';
 import Nav from './Nav.js';
 import NewPoll from './NewPoll.js';
 import Polls from './Polls.js';
 
 
  
- 
 var App = React.createClass({
-  setInitialState: function(){
-    return {
-      votingPolls: []
-    };
-  },
-  componentWillMount: function(){
-    this.setState({votingPolls: mypolls});
-  },
+  
   render: function(){
     return (
       <div>
-      <Nav polls={this.state.votingPolls} />
+      <Nav />
       {this.props.children}
       </div>
     );
