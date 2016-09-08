089e692e3b792eeae69d44c6c06666d739b47415
Andrew Boyle <ajboyle31@gmail.com>
2016-09-08 01:09:57 +0000

    working on router

--- public/source/App.js
+++ public/source/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import mypolls from './pollsJSON.js';
+import Nav from './Nav.js';
+import NewPoll from './NewPoll.js';
+import Polls from './Polls.js';
+
+
  
-ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('app')
-);
+ 
+var App = React.createClass({
+  setInitialState: function(){
+    return {
+      votingPolls: []
+    };
+  },
+  componentWillMount: function(){
+    this.setState({votingPolls: mypolls});
+  },
+  render: function(){
+    return (
+      <div>
+      <Nav polls={this.state.votingPolls} />
+      {this.props.children}
+      </div>
+    );
+  }
+}) ;
+ 
+ 
+ 
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Polls}/>
+      <Route path="newpoll" component={NewPoll} />
+    </Route>
+  </Router>
+), document.getElementById('app'));
