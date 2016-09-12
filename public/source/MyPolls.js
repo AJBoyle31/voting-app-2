5e59388f142d19fda5e99b83402cd8dd49936171
Andrew Boyle <ajboyle31@gmail.com>
2016-09-12 22:02:41 +0000

    working on filtering polls

--- public/source/MyPolls.js
+++ public/source/MyPolls.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import mypolls from './pollsJSON.js';
+//assuming the user logged in is CThePants
 
 var MyPolls = React.createClass({
+    setInitialState: function(){
+        return {
+            myPolls: []
+        };
+    },
+    componentWillMount: function(){
+        this.setState({myPolls: mypolls});
+    },
     render: function(){
-        return <h1 id="test">TESTING</h1>;
+        
+        if (this.state.myPolls){
+            var pollsResult = this.state.votingPolls.map((poll) => {
+                return <Poll pollData={poll} pollname={poll.pollname} key={poll.id} />;
+            });
+        }
+        
+        return (
+            <div id="polls">
+                <h2 id="pageTitle">FCC Voting App</h2>
+                <h4 id="pageDesc">Select a poll to see the results and vote!</h4>
+                {pollsResult}
+            </div>
+        );
     }
 });
 
